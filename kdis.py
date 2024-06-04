1fbee5785fdbbeff5da955976ff3decd06c63ee2
李四 <42721712+L4cache@users.noreply.github.com>
2024-06-04 18:16:29 +0800

    up history

--- kdis.py
+++ kdis.py
@@ -11,10 +11,11 @@ def get_file_hash(file:str,blksize:int=32<<20):
     return sha256_hash.hexdigest().lower()
 
 htm_fil = input('htm(l) file name: ')
-htm_fil = htm_fil[1:] if htm_fil.startswith('"') else htm_fil
-htm_fil = htm_fil[:-1] if htm_fil.endswith('"') else htm_fil
+htm_fil = htm_fil.strip('"')
 htm_fil_p = pathlib.Path(htm_fil).absolute()
 htm_fil_f = htm_fil_p.parent / (htm_fil_p.name.split('.')[0]+'_files')
+htm_fil_dl= htm_fil_f / 'originals'
+htm_fil_dl.mkdir(exist_ok=True)
 
 with open(htm_fil_p, encoding='utf-8') as fil:
     soup = bs4.BeautifulSoup(fil)
@@ -28,19 +29,22 @@ for i in hrefs:
 tot = len(hrefs_dd)
 
 target_fils = []
-for i in hrefs_dd:
+for n,i in enumerate(hrefs_dd):
     fname = i.split('/')[-1]
     sname, ext = fname.split('.')
-    oname = '.'.join([sname[:64], ext])
-    target_fils.append(htm_fil_f / oname)
+    # oname = '.'.join([sname[:64], ext])
+    # target_fils.append(htm_fil_f / oname)
+    oname = '.'.join([f'{n:07d}', ext])
+    target_fils.append(htm_fil_dl / oname)
 
 giveup=0
 for n,i in enumerate(zip(hrefs_dd, target_fils)):
     srv_hash = i[0].split('/')[-1].split('.')[0]
-    loc_hash = get_file_hash(str(i[1]))
-    if loc_hash == srv_hash:
-        print(i[0], 'skipped', n+1, '/', tot)
-        continue
+    if os.path.exists(i[1]):
+        loc_hash = get_file_hash(str(i[1]))
+        if loc_hash == srv_hash:
+            print(i[0], 'skipped', n+1, '/', tot)
+            continue
     downloading=1
     while downloading:
         try:
