c0bf3ef09841fa7b6d23bacf8d4113a6b86b71ae
李四 <42721712+L4cache@users.noreply.github.com>
2024-06-04 18:15:32 +0800

    get some history set up

--- kdis.py
+++ kdis.py
@@ -3,10 +3,10 @@ import bs4
 import requests
 import hashlib
 import os
-def get_file_hash(file:str):
+def get_file_hash(file:str,blksize:int=32<<20):
     sha256_hash = hashlib.sha256()
     with open(file,"rb") as f:
-        for byte_block in iter(lambda: f.read(4096),b""):
+        for byte_block in iter(lambda: f.read(blksize),b""):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest().lower()
 
@@ -23,7 +23,7 @@ hrefs = [i['href'] for i in ass]
 hrefs = [i.split('?f=')[0] for i in hrefs]
 hrefs_dd = []
 for i in hrefs:
-    if not i in hrefs_dd:
+    if not i in hrefs_dd and '/data//' in i:
         hrefs_dd.append(i)
 tot = len(hrefs_dd)
 
@@ -31,18 +31,34 @@ target_fils = []
 for i in hrefs_dd:
     fname = i.split('/')[-1]
     sname, ext = fname.split('.')
-    oname = '.'.join([sname[:-4], ext])
+    oname = '.'.join([sname[:64], ext])
     target_fils.append(htm_fil_f / oname)
 
+giveup=0
 for n,i in enumerate(zip(hrefs_dd, target_fils)):
     srv_hash = i[0].split('/')[-1].split('.')[0]
     loc_hash = get_file_hash(str(i[1]))
     if loc_hash == srv_hash:
         print(i[0], 'skipped', n+1, '/', tot)
         continue
-    resp = requests.get(i[0])
-    with open(i[1],'wb') as out:
-        out.write(resp.content)
-    print(i[0], 'downloaded', n+1, '/', tot)
-print('All files downloaded')
+    downloading=1
+    while downloading:
+        try:
+            resp = requests.get(i[0])
+            if resp.status_code != 200:
+                raise ValueError(f'response not ok, code: {resp.status_code}')
+            with open(i[1],'wb') as out:
+                out.write(resp.content)
+                downloading=0
+                print(i[0], 'downloaded', n+1, '/', tot)
+        except Exception as exc:
+            print(f'{i[0]} Exception {exc}')
+            quiz=input('retry? (1/0)')
+            if quiz=='0':
+                downloading=0
+                giveup+=1
+if not giveup:
+    print('All files downloaded')
+else:
+    print(f'{giveup} files not downloaded')
 os.system('pause')
